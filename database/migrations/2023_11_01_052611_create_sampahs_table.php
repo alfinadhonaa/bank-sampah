b55ee1e1bfa9be0c3c7701bda860defaca73ad39
alfinadhonaa <93650389+alfinadhonaa@users.noreply.github.com>
2023-11-01 15:09:00 +0700

    Update 2023_11_01_052611_create_sampahs_table.php

--- database/migrations/2023_11_01_052611_create_sampahs_table.php
+++ database/migrations/2023_11_01_052611_create_sampahs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('sampahs', function (Blueprint $table) {
             $table->increments('id_sampah');
-            $table->string('jenis_sampah');
+            $table->integer('jenis_sampah');
             $table->integer('harga');
             $table->integer('berat');
             $table->integer('total');
